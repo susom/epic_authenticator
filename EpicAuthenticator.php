d405a089ab9c5b52fcd515bd1de5986bebe6528a
ihabz <ihab.zeedia@gmail.com>
2025-12-10 13:43:43 -0800

    initial template

---
+++ EpicAuthenticator.php
@@ -0,0 +1,10 @@
+<?php
+namespace Stanford\EpicAuthenticator;
+
+class EpicAuthenticator extends \ExternalModules\AbstractExternalModule {
+    public function __construct() {
+        parent::__construct();
+        // Other code to run when object is instantiated
+    }
+
+}
